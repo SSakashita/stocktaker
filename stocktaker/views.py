75ba14fb7240c58c6f4300a308341dc9b1e60e57
sakashi <sakashita1969@gmail.com>
2020-09-22 14:34:38 +0900

    Added view and template for detailed item.

--- stocktaker/views.py
+++ stocktaker/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from .models import Item
@@ -7,3 +7,7 @@ from .models import Item
 def item_list(request):
   items = Item.objects.filter(stocked_date__lte=timezone.now()).order_by('stocked_date')
   return render(request, 'stocktaker/item_list.html', {'items': items})
+
+def item_detail(request, pk):
+  item = get_object_or_404(Item, pk=pk)
+  return render(request, 'stocktaker/item_detail.html', {'item': item})
