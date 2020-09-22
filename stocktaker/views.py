2e8bdcc3ffd091b27f93dc930e40e3ee3d3dc329
sakashi <sakashita1969@gmail.com>
2020-09-22 13:38:34 +0900

    Add base.htmlt

--- stocktaker/views.py
+++ stocktaker/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Item
+
 
 def item_list(request):
-  return render(request, 'stocktaker/item_list.html', {})
+  items = Item.objects.filter(stocked_date__lte=timezone.now()).order_by('stocked_date')
+  return render(request, 'stocktaker/item_list.html', {'items': items})
